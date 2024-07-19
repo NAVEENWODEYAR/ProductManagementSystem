1e90881a31caa63ae21717ea356d326172247afe
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-19 17:55:56 +0530

    inventory-service app added

---
+++ inventory-service/src/main/java/com/pms/InventoryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+		System.out.println("inventory\nservicE");
+	}
+
+}

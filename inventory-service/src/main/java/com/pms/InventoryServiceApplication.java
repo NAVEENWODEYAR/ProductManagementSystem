7cf5368e0ecf0d897f3e321082c4277c7b2bc01a
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-19 23:03:45 +0530

    inventory-service api tested

--- inventory-service/src/main/java/com/pms/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/pms/InventoryServiceApplication.java
@@ -1,7 +1,12 @@
 package com.pms;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.pms.entity.Inventory;
+import com.pms.repository.InventoryRepository;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -9,6 +14,21 @@ public class InventoryServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryServiceApplication.class, args);
 		System.out.println("inventory\nservicE");
+	}
+	
+	@Bean
+	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+		return args->{
+			Inventory inventory = new Inventory();
+					  inventory.setSkuCode("Iphone 16");
+					  inventory.setQuantity(10);
+		  Inventory inventory1 = new Inventory();
+					  inventory1.setSkuCode("Iphone 16Plus");
+					  inventory1.setQuantity(10);
+					  
+					  inventoryRepository.save(inventory);
+					  inventoryRepository.save(inventory1);
+		};
 	}
 
 }

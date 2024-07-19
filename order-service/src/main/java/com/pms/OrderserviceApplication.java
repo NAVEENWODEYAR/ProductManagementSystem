3bc299e2872d7a64eff6340f1b37b4b350d7a50f
Naveen <naveenkrishnawodeyar@gmail.com>
2024-07-19 13:54:46 +0530

    order service added

---
+++ order-service/src/main/java/com/pms/OrderserviceApplication.java
@@ -0,0 +1,14 @@
+package com.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderserviceApplication.class, args);
+		System.out.println("ordeR\nservicE");
+	}
+
+}
